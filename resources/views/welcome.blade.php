aaa84d3d4203f235df3daea4006b6e4b9755f8a8
nereafh <nereafernandezh@gmail.com>
2026-05-14 01:30:23 +0200

    prueba vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,6 @@
 @section('titulo', 'GeN Trading')
 @section('contenido')
 
-<div id="test-vue" style="padding: 100px; text-align: center; font-size: 2rem;">
-    Cargando Vue...
-</div>
-
-<script>
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        var el = document.getElementById('test-vue');
-        var appEl = document.getElementById('app');
-        if (appEl && appEl.__vue_app__) {
-            el.innerHTML = '✅ Vue está funcionando';
-            el.style.color = 'green';
-        } else {
-            el.innerHTML = '❌ Vue NO está montado - Error en JS';
-            el.style.color = 'red';
-        }
-    }, 3000);
-});
-</script>
+    <portada-section></portada-section>
 
 @endsection
