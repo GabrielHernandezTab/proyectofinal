4af31756249948831f5a9de4346461a56e244eb3
nereafh <nereafernandezh@gmail.com>
2026-05-14 01:39:47 +0200

    archivo completo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,4 +12,20 @@
 
     <elegirnos-section></elegirnos-section>
 
+    <div style="background-color: white; height: 100px; width: 100%;"></div>
+
+    <planes-section></planes-section>
+
+    <div style="background-color: white; height: 100px; width: 100%;"></div>
+
+    <donaciones-section></donaciones-section>
+
+    <div style="background-color: white; height: 100px; width: 100%;"></div>
+
+    <anuncios-section></anuncios-section>
+
+    <div style="background-color: white; height: 100px; width: 100%;"></div>
+
+    <footer-section></footer-section>
+
 @endsection
